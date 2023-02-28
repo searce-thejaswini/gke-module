5a29b56b024d9e5e55218d8d1d1570d20c509ff8
searce-thejaswini <107244346+searce-thejaswini@users.noreply.github.com>
2023-02-28 11:01:16 +0530

    Update main.tf

--- privatecluster1/main.tf
+++ privatecluster1/main.tf
@@ -6,11 +6,11 @@ resource "google_container_cluster" "primary" {
   network                   = var.gkes.network
   subnetwork                = var.gkes.subnetwork
   project                   = var.gkes.project_id
-  remove_default_node_pool  = var.gkes.remove_default_node_pool
+  remove_default_node_pool  = true
   provider                  = google-beta
 
   release_channel {
-    channel = var.gkes.release_channel
+    channel = "STABLE"
   }
 
   ip_allocation_policy {
@@ -22,24 +22,24 @@ resource "google_container_cluster" "primary" {
   }
 
   cluster_autoscaling {
-    enabled = var.gkes.cluster_autoscaling
+    enabled = true
     resource_limits {
-      resource_type = var.gkes.resource_type_cpu
-      minimum       = var.gkes.minimum_resource_limit_cpu
-      maximum       = var.gkes.maximum_resource_limit_cpu
+      resource_type = "cpu"
+      minimum       = 6
+      maximum       = 20
     }
     resource_limits {
-      resource_type = var.gkes.resource_type_memory
-      minimum       = var.gkes.minimum_resource_limit_memory
-      maximum       = var.gkes.maximum_resource_limit_memory
+      resource_type = "memory"
+      minimum       = 24
+      maximum       = 80
     }
   }
 
   binary_authorization {
-    evaluation_mode = var.gkes.evaluation_mode # "PROJECT_SINGLETON_POLICY_ENFORCE"
+    evaluation_mode = var.gkes.evaluation_mode
   }
 
-  enable_shielded_nodes = var.gkes.enable_shielded_nodes
+  enable_shielded_nodes = true
   initial_node_count    = var.gkes.initial_node_count
   networking_mode       = var.gkes.networking_mode
   logging_service       = var.gkes.logging_service
@@ -58,8 +58,8 @@ resource "google_container_cluster" "primary" {
       for private_cluster_config in var.gkes.private_cluster_config : private_cluster_config
     ]
     content {
-      enable_private_endpoint = private_cluster_config.value.enable_private_endpoint
-      enable_private_nodes    = private_cluster_config.value.enable_private_nodes
+      enable_private_endpoint = true
+      enable_private_nodes    = true
       master_ipv4_cidr_block  = private_cluster_config.value.master_ipv4_cidr_block
     }
   }
@@ -78,7 +78,7 @@ resource "google_container_cluster" "primary" {
       enabled = var.gkes.dns_cache_config
     }
   }
-  datapath_provider = var.gkes.datapath_provider
+  datapath_provider = "ADVANCED_DATAPATH"
 }
 
 resource "google_container_node_pool" "primary_preemptible_nodes" {
@@ -106,8 +106,8 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
     labels = var.gke_node_variables.labels
 
     shielded_instance_config {
-      enable_secure_boot          = var.gke_node_variables.enable_secure_boot
-      enable_integrity_monitoring = var.gke_node_variables.enable_integrity_monitoring
+      enable_secure_boot          = true
+      enable_integrity_monitoring = true
     }
   }
 }
